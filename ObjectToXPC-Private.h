e108cf3f98a9788a458535b4f72738995af88202
Aron Cedercrantz <aron@cedercrantz.se>
2011-12-17 16:54:47 +0100

    Add the private header required by all categories.
    - Oops, should have been added first of all.
    
    - This makes the CD_FIX_CATEGORY_BUG(name) macro available. The macro makes it possible to load the categories without adding extra options to the linker.

--- ObjectToXPC-Private.h
+++ ObjectToXPC-Private.h
@@ -1,14 +1,25 @@
 //
-//  CollectionToXPC-Private.h
-//  CollectionToXPC
+//  ObjectToXPC-Private.h
+//  ObjectToXPC
 //
 //  Created by Aron Cedercrantz on 2011-12-16.
-//  Copyright (c) 2011 Icomera. All rights reserved.
+//  Copyright (c) 2011 Aron Cedercrantz. All rights reserved.
 //
 
-#ifndef CollectionToXPC_CollectionToXPC_Private_h
-#define CollectionToXPC_CollectionToXPC_Private_h
+#ifndef ObjectToXPC_CollectionToXPC_Private_h
+#define ObjectToXPC_CollectionToXPC_Private_h
 
-
+/**
+ * Force a category to be loaded when an app starts up.
+ *
+ * Add this macro before each category implementation, so we don't have to use
+ * -all_load or -force_load to load object files from static libraries that only contain
+ * categories and no classes.
+ * See http://developer.apple.com/library/mac/#qa/qa2006/qa1490.html for more info.
+ *
+ * Macro from the Numbis project at https://github.com/jverkoey/nimbus/.
+ */
+#define CD_FIX_CATEGORY_BUG(name) @interface CD_FIX_CATEGORY_BUG_##name @end \
+@implementation CD_FIX_CATEGORY_BUG_##name @end
 
 #endif
