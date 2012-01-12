4f6d9ae921771721b1ee8a6e55ab2cf4798fecb7
Aron Cedercrantz <aron@cedercrantz.se>
2012-01-12 02:44:21 +0100

    Add framework target and NSNull category.
    - Due to the framework target the source code have been re-organized.
    
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

--- ObjectToXPC/ObjectToXPC.h
+++ ObjectToXPC/ObjectToXPC.h
@@ -28,30 +28,12 @@
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of Aron Cedercrantz.
  */
-#import "NSString+CDXPC.h"
 
-#import "ObjectToXPC-Private.h"
+#import <ObjectToXPC/NSArray+CDXPC.h>
+#import <ObjectToXPC/NSData+CDXPC.h>
+#import <ObjectToXPC/NSDate+CDXPC.h>
+#import <ObjectToXPC/NSDictionary+CDXPC.h>
+#import <ObjectToXPC/NSNumber+CDXPC.h>
+#import <ObjectToXPC/NSString+CDXPC.h>
+#import <ObjectToXPC/NSNull+CDXPC.h>
 
-CD_FIX_CATEGORY_BUG(NSString_CDXPC)
-@implementation NSString (CDXPC)
-
-+ (id)stringWithXPCObject:(xpc_object_t)xpcObject
-{
-	return [[[self class] alloc] initWithXPCObject:xpcObject];
-}
-
-- (id)initWithXPCObject:(xpc_object_t)xpcObject
-{
-	NSAssert(xpc_get_type(xpcObject) == XPC_TYPE_STRING, @"xpcObject must be of type XPC_TYPE_STRING");
-	
-	const char *xpcString = xpc_string_get_string_ptr(xpcObject);
-	return [self initWithUTF8String:xpcString];
-}
-
-- (xpc_object_t)XPCObject
-{
-	xpc_object_t resXpcString = xpc_string_create([self UTF8String]);
-	return resXpcString;
-}
-
-@end
